a32247323f13ac581774c51fe8b7a4b1e5d3caa6
Matheus Bastos <matheus.mab432@gmail.com>
2023-06-29 20:04:09 -0300

    adding clip fields

--- projects/clipstash/src/lib/domain/clip/mod.rs
+++ projects/clipstash/src/lib/domain/clip/mod.rs
@@ -1,1 +1,34 @@
 pub mod field;
+
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+#[derive(Clone, Debug, Error)]
+pub enum ClipErr {
+    #[error("invalid password: {0}")]
+    InvalidPassword(String),
+    #[error("invalid title: {0}")]
+    InvalidTitle(String),
+    #[error("empty content")]
+    EmptyContent,
+    #[error("invalid date: {0}")]
+    InvalidDate(String),
+    #[error("date parse error: {0}")]
+    DateParse(#[from] chrono::ParseError),
+    #[error("id parse error: {0}")]
+    Id(#[from] uuid::Error),
+    #[error("hits parse error: {0}")]
+    Hits(#[from] std::num::TryFromIntError),
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Clip {
+    pub clip_id: field::ClipId,
+    pub shortcode: field::Shortcode,
+    pub content: field::Content,
+    pub title: field::Title,
+    pub posted: field::Posted,
+    pub expires: field::Expires,
+    pub password: field::Password,
+    pub hits: field::Hits,
+}
